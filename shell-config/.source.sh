b91261cc1fb8d9e4a7da9bedb794c6ddd4cb10c1
Sebastian Wilgosz <sebastian.wilgosz@gmail.com>
2016-03-29 17:31:41 +0200

    Added git aliasses

--- shell-config/.source.sh
+++ shell-config/.source.sh
@@ -1,6 +1,6 @@
 source $HOME/bin/shell-config/git-branch-color.sh
 source $HOME/bin/shell-config/git-completion.sh
-
+source $HOME/bin/shell-config/.bash_aliases
 # Base16 Shell
 BASE16_SHELL="$HOME/bin/base16-shell/base16-monokai.dark.sh"
 [[ -s $BASE16_SHELL ]] && source $BASE16_SHELL
