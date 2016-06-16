58b42941c5cb21af8bae577d3f4f193d2e159110
Sebastian Wilgosz <sebastian.wilgosz@useo.pl>
2016-06-16 22:33:40 +0200

    Tmux config improvements
    - tmux scrolling fix
    - tmux workflow change to multiple windows instead of panes

---
+++ tmux/paneized.sh
@@ -0,0 +1,22 @@
+tmux new-session -d -s $session
+tmux rename-window -t "${session}:0" "shell"
+
+#vim
+tmux new-window -n vim
+tmux send-keys "${prcmd}; vim" C-m
+
+#panel layout preparation
+tmux select-window -t ${window1}
+tmux split-window -h -p 50
+tmux select-pane -t ${window1}.0
+tmux split-window -v -p 50
+
+#commands
+tmux send-keys -t ${window1}.0 "${prcmd}; rails c" C-m
+tmux send-keys -t ${window1}.1 "${prcmd}; ${scmd}" C-m
+tmux send-keys -t ${window1}.2 $prcmd C-m
+
+tmux select-pane -t ${window1}.2
+tmux select-window -t ${window2}
+tmux attach -t ${session}
+
--- tmux/rails-dev.sh
+++ tmux/rails-dev.sh
@@ -1,22 +1,27 @@
+
 tmux new-session -d -s $session
-tmux rename-window -t "${session}:0" "shell"
+
+#shell window
+# tmux rename-window -t "${session}:0" "shell"
+tmux send-keys -t "${session}:0" $prcmd C-m
 
 #vim
 tmux new-window -n vim
-tmux send-keys "${prcmd}; vim" C-m
+tmux send-keys -t "${session}:1" "${prcmd}; vim" C-m
 
-#panel layout preparation
-tmux select-window -t ${window1}
+# #rails console and server
+tmux new-window -n rails
+tmux send-keys -t "${session}:2" "${prcmd}; ${scmd}" C-m
+tmux select-window -t "${session}:2"
 tmux split-window -h -p 50
-tmux select-pane -t ${window1}.0
-tmux split-window -v -p 50
+tmux send-keys -t ${session}:2.1 "${prcmd}; rails c" C-m
 
-#commands
-tmux send-keys -t ${window1}.0 "${prcmd}; rails c" C-m
-tmux send-keys -t ${window1}.1 "${prcmd}; ${scmd}" C-m
-tmux send-keys -t ${window1}.2 $prcmd C-m
+# #focus on vim
+tmux select-window -t ${session}:1
 
-tmux select-pane -t ${window1}.2
-tmux select-window -t ${window2}
+tmux rename-window -t "${session}:0" "shell"
+tmux rename-window -t "${session}:1" "vim"
+tmux rename-window -t "${session}:2" "rails"
+#run all
 tmux attach -t ${session}
 
